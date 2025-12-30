9e1bea8e136be0bed16f530063c64ece60d375b8
soaresmmsx-collab <soaresmmsx@gmail.com>
2025-12-30 17:11:41 -0300

    Create tabelas_simples.py

---
+++ tabelas_simples.py
@@ -0,0 +1,11 @@
+TABELA_SIMPLES = [
+    {"anexo": "III", "min": 0, "max": 180000, "aliquota": 0.06, "deducao": 0},
+    {"anexo": "III", "min": 180000.01, "max": 360000, "aliquota": 0.112, "deducao": 9360},
+    {"anexo": "III", "min": 360000.01, "max": 720000, "aliquota": 0.135, "deducao": 17640},
+    {"anexo": "III", "min": 720000.01, "max": 1800000, "aliquota": 0.16, "deducao": 35640},
+
+    {"anexo": "V", "min": 0, "max": 180000, "aliquota": 0.155, "deducao": 0},
+    {"anexo": "V", "min": 180000.01, "max": 360000, "aliquota": 0.18, "deducao": 4500},
+    {"anexo": "V", "min": 360000.01, "max": 720000, "aliquota": 0.195, "deducao": 9900},
+    {"anexo": "V", "min": 720000.01, "max": 1800000, "aliquota": 0.205, "deducao": 17100},
+]
